3b34b41295afdaa944bdde8228434cf02a8d3401
PauloVFC94 <paulovictorfcosta@gmail.com>
2023-01-25 12:49:41 -0300

    Script do formulário inicio

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const submitBtn = document.getElementsByClassName("submit-btn");
+const submitBtn = document.querySelector('.submit-btn');
 const stackInput = document.querySelectorAll(".stack-input");
-stackInput[1].checked = true;
-console.log(stackInput[1].value);
-console.log(stackInput[1]);
-console.log(submitBtn);
-
-const checkStack = () => {
-    for (let i = 0; i < stackInput.length; i++) {
-        if (stackInput[i].checked) {
-            console.log(stackInput).value;
-            return stackInput[i].value;
-        }
-    }
-}
-
-const tinoco = checkStack();
+const name = document.querySelector('.name-field');
+const email = document.querySelector('.email-field');
 
 
-submitBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log(submitBtn);
-})
+submitBtn.addEventListener('click', () => console.log(`${name.value} e ${email.value}`));
